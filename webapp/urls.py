3d37eff817d1187c1f78251f91c007d7e456358e
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2024-04-24 12:15:10 +0600

    Re-structured the urls.py of the project
    added separate urls.py for each app applying encapsulation

--- webapp/urls.py
+++ webapp/urls.py
@@ -14,23 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
-from home.views import delete_res, receipes, update_res
-from index.views import inde
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-urlpatterns = [
-     path('',inde),
-     path('h/',receipes), #receipes is funtion name#
-     path("dr/<id>/", delete_res , name = "delete_res"),
-     path("ur/<id>/", update_res , name = "update_res"),
-    path('admin/', admin.site.urls),
-    
-   
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("index.urls")),
+    path("auth/dashbord/", include("home.urls"))
 ]
 
 if settings.DEBUG:
